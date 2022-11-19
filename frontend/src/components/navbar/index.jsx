4540e087cc685d3348ea5446e97e3273b75a9bb4
Daniel O Sousa <daniel-sousa.ds@academico.ifpb.edu.br>
2022-11-19 15:25:31 -0300

    Adicionando componente navbar

---
+++ frontend/src/components/navbar/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.css'
+
+export function Navbar() {
+  return (
+    <nav>
+      <ul className="nav-menu">
+        <li>
+          <a href="/" title="Ir para home">Home</a>
+        </li>
+        <li>
+          <a href="/list_all" title="Ir para lista de hemonúcleos">Hemos List</a>
+        </li>
+        <li>
+          <a href="/add" title="Ir para o mapa">Hemos Map</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
